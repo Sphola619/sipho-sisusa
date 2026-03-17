a8588cc36a2f7083ec8ab45b8b6285e7c44eb4ba
Sipho <sisusasipho@gmail.com>
2026-03-17 18:40:18 +0200

    background style update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -5,17 +6,44 @@ import TechStack from "./components/TechStack";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+const stars = Array.from({ length: 80 }, (_, index) => ({
+  id: index,
+  top: `${Math.random() * 100}%`,
+  left: `${Math.random() * 100}%`,
+  size: `${Math.random() * 2 + 1}px`,
+  duration: `${Math.random() * 3 + 3}s`,
+  delay: `${Math.random() * 4}s`,
+}));
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <About />
-      <TechStack />
-      <Projects />
-      <Contact />
-    </>
+    <div className="app-shell">
+      <div className="page-stars" aria-hidden="true">
+        {stars.map((star) => (
+          <span
+            key={star.id}
+            className="page-star"
+            style={{
+              top: star.top,
+              left: star.left,
+              width: star.size,
+              height: star.size,
+              animationDuration: star.duration,
+              animationDelay: star.delay,
+            }}
+          />
+        ))}
+      </div>
+
+      <div className="app-content">
+        <Navbar />
+        <Hero />
+        <About />
+        <TechStack />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
   );
 }
 
